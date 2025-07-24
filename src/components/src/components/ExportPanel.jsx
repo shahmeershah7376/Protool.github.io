341c8cdbd43f991c4f162ba527cd5e7b711f7333
shahmeershah7376 <tareenali907@gmail.com>
2025-07-24 10:55:17 +0500

    Create ExportPanel.jsx

---
+++ src/components/src/components/ExportPanel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function CanvasArea() {
+  return (
+    <div className="flex-grow bg-gray-200 flex items-center justify-center">
+      <div className="w-3/4 h-3/4 bg-white shadow-md flex items-center justify-center">
+        <p className="text-gray-400">Canvas / Video Editor</p>
+      </div>
+    </div>
+  );
+}
